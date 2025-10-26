e284eaee2f5c0fab417858e06ff112c21ed7bc20
prabhakar <prabhakarpandey7061@gmail.com>
2025-10-26 18:27:20 +0530

    added header and logo

---
+++ portfolio/TODO.md
@@ -0,0 +1,8 @@
+# TODO: Fix Header Menu Toggle
+
+- [x] Import useState from React in Header.jsx
+- [x] Add state variable `isMenuOpen` initialized to false
+- [x] Add toggleMenu function to toggle the state
+- [x] Update the button with onClick handler and dynamic aria-expanded
+- [x] Update the menu div className to conditionally show/hide based on state
+- [x] Test the menu toggle by running the development server and verifying functionality (server running on http://localhost:5175/)
--- portfolio/src/components/logo/Logo.jsx
+++ portfolio/src/components/logo/Logo.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React,{useRef , useEffect} from 'react'
+import { Link } from 'react-router';
 
 function Logo() {
+ const logoRef = useRef();
+ useEffect(()=>{
+  logoRef.current.style.transform = "all 0.3s ease";
+  logoRef.current.style.cuesor="pointer"
+ })
+
+
   return (
     <div>
+      <Link to = "/">
       
+      <div ref={logoRef} className="flex text-amber-600 text-2xl font-bold">
+        My<span className="text-amber-400">Portfolio</span>
+      </div>
+
+      </Link>
     </div>
   )
 }
