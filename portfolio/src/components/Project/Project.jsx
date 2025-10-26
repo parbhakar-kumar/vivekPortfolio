55f796c50284b15ee7ecf908684d83190b25ecab
prabhakar <prabhakarpandey7061@gmail.com>
2025-10-26 18:06:16 +0530

    aded  route

---
+++ portfolio/src/components/Project/Project.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <div>
+      this is project page
+    </div>
+  )
+}
+
+export default Project
--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,10 +1,47 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/home/Home.jsx'
+import Layout from './Layout/Layout.jsx'
+import About from './components/about/About.jsx'
+import Project from './components/Project/Project.jsx'
+import Contect from './components/conteact/Contect.jsx'
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"project",
+        element:<Project/>
+      },
+      {
+        path:"contact",
+        element:<Contect/>
+      }
+    ]
+  }
+])
+
+
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
